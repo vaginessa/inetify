a18d32093294d4b4fd31ec7d7b5e10e78a0018ed
gmdode@gmail.com <gmdode@gmail.com@71b85988-53df-8230-a4b4-4f5589cecf00>
2011-03-16 02:22:14 +0000

    Added a description to the main view

--- inetify/inetify-app/gen/net/luniks/android/inetify/R.java
+++ inetify/inetify-app/gen/net/luniks/android/inetify/R.java
@@ -23,14 +23,15 @@ public final class R {
     }
     public static final class id {
         public static final int linearLayoutMain=0x7f090000;
-        public static final int refresh=0x7f090007;
-        public static final int settings=0x7f090008;
-        public static final int tableLayoutInfo=0x7f090003;
-        public static final int tablerow_info=0x7f090004;
-        public static final int textview_info_property=0x7f090005;
-        public static final int textview_info_value=0x7f090006;
+        public static final int refresh=0x7f090008;
+        public static final int settings=0x7f090009;
+        public static final int tableLayoutInfo=0x7f090004;
+        public static final int tablerow_info=0x7f090005;
+        public static final int textview_description=0x7f090002;
+        public static final int textview_info_property=0x7f090006;
+        public static final int textview_info_value=0x7f090007;
         public static final int textview_name=0x7f090001;
-        public static final int view_spacer=0x7f090002;
+        public static final int view_spacer=0x7f090003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -41,37 +42,39 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f060000;
-        public static final int default_value_enabled_preference=0x7f060008;
-        public static final int default_value_light_preference=0x7f06000d;
-        public static final int default_value_server_preference=0x7f060010;
-        public static final int default_value_title_preference=0x7f060014;
-        public static final int dialog_title_server_preference=0x7f060013;
-        public static final int dialog_title_title_preference=0x7f060017;
+        public static final int default_value_enabled_preference=0x7f060009;
+        public static final int default_value_light_preference=0x7f06000e;
+        public static final int default_value_server_preference=0x7f060011;
+        public static final int default_value_title_preference=0x7f060015;
+        public static final int description=0x7f060002;
+        public static final int dialog_title_server_preference=0x7f060014;
+        public static final int dialog_title_title_preference=0x7f060018;
         public static final int hello=0x7f060001;
-        public static final int notification_nok_text=0x7f060006;
-        public static final int notification_nok_title=0x7f060005;
-        public static final int notification_ok_text=0x7f060004;
-        public static final int notification_ok_title=0x7f060003;
-        public static final int service_label=0x7f060002;
-        public static final int settings_label=0x7f060007;
-        public static final int summary_enabled_preference=0x7f06000a;
-        public static final int summary_light_preference=0x7f06000f;
-        public static final int summary_server_preference=0x7f060012;
-        public static final int summary_title_preference=0x7f060016;
-        public static final int summary_tone_preference=0x7f06000c;
-        public static final int title_enabled_preference=0x7f060009;
-        public static final int title_light_preference=0x7f06000e;
-        public static final int title_server_preference=0x7f060011;
-        public static final int title_title_preference=0x7f060015;
-        public static final int title_tone_preference=0x7f06000b;
+        public static final int notification_nok_text=0x7f060007;
+        public static final int notification_nok_title=0x7f060006;
+        public static final int notification_ok_text=0x7f060005;
+        public static final int notification_ok_title=0x7f060004;
+        public static final int service_label=0x7f060003;
+        public static final int settings_label=0x7f060008;
+        public static final int summary_enabled_preference=0x7f06000b;
+        public static final int summary_light_preference=0x7f060010;
+        public static final int summary_server_preference=0x7f060013;
+        public static final int summary_title_preference=0x7f060017;
+        public static final int summary_tone_preference=0x7f06000d;
+        public static final int title_enabled_preference=0x7f06000a;
+        public static final int title_light_preference=0x7f06000f;
+        public static final int title_server_preference=0x7f060012;
+        public static final int title_title_preference=0x7f060016;
+        public static final int title_tone_preference=0x7f06000c;
     }
     public static final class style {
         public static final int LinearLayoutMain=0x7f070000;
-        public static final int TableLayoutInfo=0x7f070003;
-        public static final int TextViewInfoProperty=0x7f070004;
-        public static final int TextViewInfoValue=0x7f070005;
+        public static final int TableLayoutInfo=0x7f070004;
+        public static final int TextViewDescription=0x7f070002;
+        public static final int TextViewInfoProperty=0x7f070005;
+        public static final int TextViewInfoValue=0x7f070006;
         public static final int TextViewName=0x7f070001;
-        public static final int ViewSpacer=0x7f070002;
+        public static final int ViewSpacer=0x7f070003;
     }
     public static final class xml {
         public static final int settings=0x7f040000;
