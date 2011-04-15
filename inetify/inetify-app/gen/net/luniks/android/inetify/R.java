31fb51113afb70f365b65d2a3ecf58f88d320fca
gmdode@gmail.com <gmdode@gmail.com@71b85988-53df-8230-a4b4-4f5589cecf00>
2011-04-15 18:15:34 +0000

    Added German translation

--- inetify/inetify-app/gen/net/luniks/android/inetify/R.java
+++ inetify/inetify-app/gen/net/luniks/android/inetify/R.java
@@ -8,10 +8,6 @@
 package net.luniks.android.inetify;
 
 public final class R {
-    public static final class array {
-        public static final int entries_interval_preference=0x7f050000;
-        public static final int entryvalues_interval_preference=0x7f050001;
-    }
     public static final class attr {
     }
     public static final class drawable {
@@ -23,79 +19,79 @@ public final class R {
         public static final int notification_ok=0x7f020005;
     }
     public static final class id {
-        public static final int button_test=0x7f090008;
-        public static final int help=0x7f09000c;
-        public static final int linearlayout_button_test=0x7f090007;
-        public static final int linearlayout_help=0x7f090000;
-        public static final int linearlayout_main=0x7f090003;
-        public static final int scrollview_help=0x7f090001;
-        public static final int settings=0x7f09000d;
-        public static final int textview_connection=0x7f09000a;
-        public static final int textview_description=0x7f090005;
-        public static final int textview_help=0x7f090002;
-        public static final int textview_info=0x7f09000b;
-        public static final int textview_name=0x7f090004;
-        public static final int view_spacer1=0x7f090006;
-        public static final int view_spacer2=0x7f090009;
+        public static final int button_test=0x7f080008;
+        public static final int help=0x7f08000c;
+        public static final int linearlayout_button_test=0x7f080007;
+        public static final int linearlayout_help=0x7f080000;
+        public static final int linearlayout_main=0x7f080003;
+        public static final int scrollview_help=0x7f080001;
+        public static final int settings=0x7f08000d;
+        public static final int textview_connection=0x7f08000a;
+        public static final int textview_description=0x7f080005;
+        public static final int textview_help=0x7f080002;
+        public static final int textview_info=0x7f08000b;
+        public static final int textview_name=0x7f080004;
+        public static final int view_spacer1=0x7f080006;
+        public static final int view_spacer2=0x7f080009;
     }
     public static final class layout {
         public static final int help=0x7f030000;
         public static final int main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f060000;
-        public static final int button_test=0x7f060003;
-        public static final int default_value_enabled_preference=0x7f060015;
-        public static final int default_value_light_preference=0x7f06001a;
-        public static final int default_value_server_preference=0x7f06001d;
-        public static final int default_value_title_preference=0x7f060021;
-        public static final int description=0x7f060002;
-        public static final int dialog_title_server_preference=0x7f060020;
-        public static final int dialog_title_title_preference=0x7f060024;
-        public static final int empty=0x7f06000c;
-        public static final int hello=0x7f060001;
-        public static final int help=0x7f060013;
-        public static final int help_label=0x7f060012;
-        public static final int inetify_connection_string=0x7f060005;
-        public static final int inetify_connection_string_no_connection=0x7f060006;
-        public static final int inetify_error=0x7f060009;
-        public static final int inetify_error_message=0x7f06000a;
-        public static final int inetify_info_string_nok=0x7f060008;
-        public static final int inetify_info_string_ok=0x7f060007;
-        public static final int inetify_ok=0x7f060004;
-        public static final int inetify_testing=0x7f06000b;
-        public static final int notification_nok_text=0x7f060011;
-        public static final int notification_nok_title=0x7f060010;
-        public static final int notification_ok_text=0x7f06000f;
-        public static final int notification_ok_title=0x7f06000e;
-        public static final int service_label=0x7f06000d;
-        public static final int settings_label=0x7f060014;
-        public static final int summary_enabled_preference=0x7f060017;
-        public static final int summary_light_preference=0x7f06001c;
-        public static final int summary_server_preference=0x7f06001f;
-        public static final int summary_title_preference=0x7f060023;
-        public static final int summary_tone_preference=0x7f060019;
-        public static final int title_enabled_preference=0x7f060016;
-        public static final int title_light_preference=0x7f06001b;
-        public static final int title_server_preference=0x7f06001e;
-        public static final int title_title_preference=0x7f060022;
-        public static final int title_tone_preference=0x7f060018;
+        public static final int app_name=0x7f050000;
+        public static final int button_test=0x7f050003;
+        public static final int default_value_enabled_preference=0x7f050015;
+        public static final int default_value_light_preference=0x7f05001a;
+        public static final int default_value_server_preference=0x7f05001d;
+        public static final int default_value_title_preference=0x7f050021;
+        public static final int description=0x7f050002;
+        public static final int dialog_title_server_preference=0x7f050020;
+        public static final int dialog_title_title_preference=0x7f050024;
+        public static final int empty=0x7f05000c;
+        public static final int hello=0x7f050001;
+        public static final int help=0x7f050013;
+        public static final int help_label=0x7f050012;
+        public static final int inetify_connection_string=0x7f050005;
+        public static final int inetify_connection_string_no_connection=0x7f050006;
+        public static final int inetify_error=0x7f050009;
+        public static final int inetify_error_message=0x7f05000a;
+        public static final int inetify_info_string_nok=0x7f050008;
+        public static final int inetify_info_string_ok=0x7f050007;
+        public static final int inetify_ok=0x7f050004;
+        public static final int inetify_testing=0x7f05000b;
+        public static final int notification_nok_text=0x7f050011;
+        public static final int notification_nok_title=0x7f050010;
+        public static final int notification_ok_text=0x7f05000f;
+        public static final int notification_ok_title=0x7f05000e;
+        public static final int service_label=0x7f05000d;
+        public static final int settings_label=0x7f050014;
+        public static final int summary_enabled_preference=0x7f050017;
+        public static final int summary_light_preference=0x7f05001c;
+        public static final int summary_server_preference=0x7f05001f;
+        public static final int summary_title_preference=0x7f050023;
+        public static final int summary_tone_preference=0x7f050019;
+        public static final int title_enabled_preference=0x7f050016;
+        public static final int title_light_preference=0x7f05001b;
+        public static final int title_server_preference=0x7f05001e;
+        public static final int title_title_preference=0x7f050022;
+        public static final int title_tone_preference=0x7f050018;
     }
     public static final class style {
-        public static final int ButtonTest=0x7f070005;
-        public static final int LinearLayoutButtonTest=0x7f070004;
-        public static final int LinearLayoutHelp=0x7f070008;
-        public static final int LinearLayoutMain=0x7f070000;
-        public static final int ScrollViewHelp=0x7f070009;
-        public static final int TextViewConnection=0x7f070006;
-        public static final int TextViewDescription=0x7f070002;
-        public static final int TextViewHelp=0x7f07000a;
-        public static final int TextViewInfo=0x7f070007;
-        public static final int TextViewName=0x7f070001;
-        public static final int ViewSpacer=0x7f070003;
+        public static final int ButtonTest=0x7f060005;
+        public static final int LinearLayoutButtonTest=0x7f060004;
+        public static final int LinearLayoutHelp=0x7f060008;
+        public static final int LinearLayoutMain=0x7f060000;
+        public static final int ScrollViewHelp=0x7f060009;
+        public static final int TextViewConnection=0x7f060006;
+        public static final int TextViewDescription=0x7f060002;
+        public static final int TextViewHelp=0x7f06000a;
+        public static final int TextViewInfo=0x7f060007;
+        public static final int TextViewName=0x7f060001;
+        public static final int ViewSpacer=0x7f060003;
     }
     public static final class xml {
         public static final int settings=0x7f040000;
